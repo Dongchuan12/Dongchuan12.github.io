e653e76580c3d7683d6db4c82de0ff37a4db728f
Dongchuan12 <demo_12@126.com>
2017-02-13 22:24:42 +0700

    上传文件

---
+++ js/main.js
@@ -0,0 +1,60 @@
+/* 
+* @Author: anchen
+* @Date:   2017-01-04 23:03:48
+* @Last Modified by:   anchen
+* @Last Modified time: 2017-01-04 23:09:20
+*/
+
+$("#htm").circleProgress({
+    value:0.9,
+    size:150,
+    startAngle:-0.5 * Math.PI,
+    fill:{
+        gradient:["#000","#444"]
+    }
+});
+
+$("#cs").circleProgress({
+    value:0.85,
+    size:150,
+    startAngle:-0.5 * Math.PI,
+    fill:{
+        gradient:["#000","#444"]
+    }
+});
+
+$("#js").circleProgress({
+    value:0.8,
+    size:150,
+    startAngle:-0.5 * Math.PI,
+    fill:{
+        gradient:["#000","#444"]
+    }
+});
+
+$("#jq").circleProgress({
+    value:0.75,
+    size:150,
+    startAngle:-0.5 * Math.PI,
+    fill:{
+        gradient:["#000","#444"]
+    }
+});
+
+$("#ps").circleProgress({
+    value:0.7,
+    size:150,
+    startAngle:-0.5 * Math.PI,
+    fill:{
+        gradient:["#000","#444"]
+    }
+});
+
+$("#aj").circleProgress({
+    value:0.65,
+    size:150,
+    startAngle:-0.5 * Math.PI,
+    fill:{
+        gradient:["#000","#444"]
+    }
+});
